d91ab569de5773bdaa1f65c2ac1c352b8e77615d
reeFridge <reefridgerator@gmail.com>
2017-08-30 18:16:15 +0700

    Refactored build script

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/bash
 
-rm -rf ./build
+BUILD_DIR=./build
+
+if [ -d "$BUILD_DIR" ]; then
+	echo -e "[build]\tCleanup ./build dir";
+	rm -rf $BUILD_DIR;
+fi
+echo -e "[build]\tCompiling js sources"
 java -jar ./gcc/closure-compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js ./js/src/main.js --js_output_file ./js/main.js
 cobalt build
-cobalt import -b master
-git checkout master
-git push origin master
+echo -e "[build]\tDone\n"
 
-git checkout source
+# Deploy
+echo -e "To deploy build to gh-pages run:\n
+> cobalt import -b master
+> git checkout master
+> git push origin master"
+
